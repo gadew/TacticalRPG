3bf4aca34c948a308d44905f793c8fd0cbf83796
Firewill <9394155+TK-firewill@users.noreply.github.com>
2025-08-05 02:57:56 -0300

    mark click_tile_at private

--- Scenes/Pathfinding/pathfinding.gd
+++ Scenes/Pathfinding/pathfinding.gd
@@ -23,12 +23,12 @@ func _setup_astargrid2d(_astar_grid: AStarGrid2D, _terrain: TileMapLayer):
 func _ready() -> void:
     _setup_astargrid2d(astar, terrain)
 
-func click_tile_at(input_global_position: Vector2):
+func _click_tile_at(input_global_position: Vector2):
     var grid_location: Vector2i = terrain.local_to_map(terrain.to_local(input_global_position))
     unit.move_to(astar, grid_location)
     await unit.move_end
 
 func _unhandled_input(event: InputEvent) -> void:
     if event is InputEventMouseButton and event.is_pressed() and event.button_index == MOUSE_BUTTON_LEFT:
-        click_tile_at(event.position)
+        _click_tile_at(event.position)
         get_viewport().set_input_as_handled()
