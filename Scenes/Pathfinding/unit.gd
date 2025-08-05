d86aca2144512661668b6139ed7abf9bccd01fa5
Firewill <9394155+TK-firewill@users.noreply.github.com>
2025-08-05 02:27:18 -0300

    empty path handling

--- Scenes/Pathfinding/unit.gd
+++ Scenes/Pathfinding/unit.gd
@@ -7,9 +7,10 @@ var grid_location: Vector2i = Vector2i.ZERO
 
 func move_to(pathfinding: AStarGrid2D, grid_target_location: Vector2i, duration: float = 0.25):
 	var points: PackedVector2Array = pathfinding.get_point_path(grid_location, grid_target_location)
-	var tween: Tween = create_tween()
-	for point in points:
-		tween.tween_property(self, "position", point + Vector2.ONE * 32, duration)
-	await tween.finished
-	grid_location = grid_target_location
+	if not points.is_empty():
+		var tween: Tween = create_tween()
+		for point in points:
+			tween.tween_property(self, "position", point + Vector2.ONE * 32, duration)
+		await tween.finished   
+		grid_location = grid_target_location
 	move_end.emit(self)
