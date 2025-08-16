03200814f01acb45b712f3e89cf8256adb734eef
Firewill <9394155+TK-firewill@users.noreply.github.com>
2025-08-16 12:49:11 -0300

    Abide by code order and document ui.gd

--- Scripts/Ui/ui.gd
+++ Scripts/Ui/ui.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var _player_label: Label = %player 
 
+## Displays [param commander] using its [member Commander.name] and [member Commander.color_shift].
 func set_current_commander(commander: Commander) -> void:
 	_player_label.text = commander.name
 	_player_label.label_settings.font_color = Color.from_hsv(commander.color_shift, 1, 1)
