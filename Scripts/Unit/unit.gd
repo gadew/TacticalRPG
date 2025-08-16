806117c201edbd0b9697b10cfeed80c43395f2af
Firewill <9394155+TK-firewill@users.noreply.github.com>
2025-08-16 12:23:48 -0300

    Abide by code order and document unit.gd

--- Scripts/Unit/unit.gd
+++ Scripts/Unit/unit.gd
@@ -1,12 +1,19 @@
 class_name Unit
 extends Sprite2D
+## Base class for units.
+## 
+## A unit is an agent that will interact in battle with other units be it by fighting,
+## impeding movement or some other effect on the terrain.
+
+## Signals that a moving animation has finished.
+signal move_end(Unit)
 
 const MOVERANGE: int = 3
 
-signal move_end(Unit)
-
 var _commander: Commander
 
+## Instantiate a new [Unit] under the control of [param commander].[br]
+## Returns the newly instantiated [Unit].
 static func create(commander: Commander) -> Unit:
 	var unit: Unit = load("uid://twn5dfmtjwap").instantiate()
 	unit.material.set_shader_parameter("shift", commander.color_shift)
@@ -14,15 +21,19 @@ static func create(commander: Commander) -> Unit:
 	commander.register_unit(unit)
 	return unit
 
+## Returns wether this [Unit] is controlled by [param commander].
 func is_commanded_by(commander: Commander) -> bool:
 	return _commander == commander
 
+## Mark this [Unit] as selected.
 func select() -> void:
 	%selected.visible = true
 
+## Mark this [Unit] as not selected.
 func deselect() -> void:
 	%selected.visible = false
 
+## Walk along the given [param points], taking [param duration] time to travel between each pair of points.
 func move_along(points: Array[Vector2], duration: float = 0.25) -> void:
 	var tween: Tween = create_tween()
 	for point in points:
